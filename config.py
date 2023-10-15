47f2f3c53ec204b89f787d28cfd11c773fbee148
Psn <106792309+psnprasann@users.noreply.github.com>
2023-10-15 19:36:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "15432910"))
+API_HASH = os.environ.get("API_HASH", "bf3b5a92497cf5744e6310f309b5490a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6356613407:AAFd5nLh7iNIq1Xe55e-38EjzrpAZB7gb6k")
+ADMINS = [int(i.strip()) for i in os.environ.get("5234476249").split("Owner Id")] if os.environ.get("5234476249") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Paramathma")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Paramathma:Paramathma@cluster0.qno5wbm.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5234476249")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
